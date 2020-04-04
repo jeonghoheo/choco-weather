831e24d76638d8ceefe5e5f54ab0ad0c6d556001
jeonghoheo <heojeongho1992@gmail.com>
2020-04-04 14:20:02 +0900

    add - Asking for Permissions

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 
 export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+
   getLocation = async () => {
-    const location = await Location.getCurrentPositionAsync();
-    console.log(location);
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("정보를 가져올수 없습니다.", "권한을 허락해주세요");
+    }
   };
 
   componentDidMount() {
@@ -13,6 +25,7 @@ export default class extends React.Component {
   }
 
   render() {
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
