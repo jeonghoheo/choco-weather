1f790e43614ebab13c1cc491794aa077d38cc659
jeonghoheo <heojeongho1992@gmail.com>
2020-04-04 13:39:10 +0900

    add - Loading Screen

---
+++ Loading.js
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>날씨 정보를 가져오는 중입니다...</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#FDF6AA"
+  },
+  text: {
+    color: "#2c2c2c",
+    fontSize: 30
+  }
+});
+
+export default Loading;
