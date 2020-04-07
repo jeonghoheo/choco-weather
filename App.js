1154fcc8006adf4cb56cfa4285f2b0712b1a620d
jeonghoheo <heojeongho1992@gmail.com>
2020-04-07 21:53:20 +0900

    add - Getting Weather

--- App.js
+++ App.js
@@ -2,18 +2,30 @@ import React from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+
+const API_KEY = "f748eea928b26ef6de23364577c57c9d";
+const CITY_KEY = "1835553";
 
 export default class extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+  };
+
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
   };
 
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
       const {
-        coords: { latitude, longitude }
+        coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("정보를 가져올수 없습니다.", "권한을 허락해주세요");
