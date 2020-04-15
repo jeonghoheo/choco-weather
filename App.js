b15327b442b6f63ad5f9fbdaf76d2952f5ae3aa9
jeonghoheo <heojeongho1992@gmail.com>
2020-04-15 12:04:49 +0900

    add - Title and Subtitles

--- App.js
+++ App.js
@@ -6,12 +6,10 @@ import axios from "axios";
 import Weather from "./Weather";
 
 const API_KEY = "f748eea928b26ef6de23364577c57c9d";
-const CITY_KEY = "1835553";
 
 export default class extends React.Component {
   state = {
     isLoading: true,
-    temp: undefined,
   };
 
   getWeather = async (latitude, longitude) => {
@@ -23,7 +21,8 @@ export default class extends React.Component {
     } = await axios.get(
       `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
     );
-    this.setState({ isLoading: false, condition: weather[0].main, temp: temp });
+    const newState = { isLoading: false, condition: weather[0].main, temp };
+    this.setState(newState);
   };
 
   getLocation = async () => {
@@ -33,7 +32,6 @@ export default class extends React.Component {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      this.setState({ isLoading: false });
     } catch (error) {
       Alert.alert("정보를 가져올수 없습니다.", "권한을 허락해주세요");
     }
--- Loading.js
+++ Loading.js
@@ -1,9 +1,10 @@
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 
 function Loading() {
   return (
     <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
       <Text style={styles.text}>날씨 정보를 가져오는 중입니다...</Text>
     </View>
   );
@@ -15,12 +16,12 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
     paddingHorizontal: 30,
     paddingVertical: 100,
-    backgroundColor: "#FDF6AA"
+    backgroundColor: "#FDF6AA",
   },
   text: {
     color: "#2c2c2c",
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
 
 export default Loading;
